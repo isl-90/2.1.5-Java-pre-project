73f54b64ef20c74d326d99401adfd3b3edd0c442
isl-90 <isl90@mal.ru>
2023-12-10 22:02:14 +0300

    new project

--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -7,10 +7,10 @@ import org.springframework.stereotype.Component;
 public class Ocean1 {
 
     @Autowired
-    private Island2 island;
+    private Island2 island2;
 
     @Override
     public String toString() {
-        return "на океане остров " + island.toString();
+        return "на океане остров " + island2.toString();
     }
 }
--- src/main/java/koschei/models/Wood3.java
+++ src/main/java/koschei/models/Wood3.java
@@ -1,15 +1,16 @@
 package koschei.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("wood3")
 public class Wood3 {
 
     private final Rabbit4 rabbit;
 
     @Autowired
-    public Wood3(Rabbit4 rabbit) {
+    public Wood3( Rabbit4 rabbit) {
         this.rabbit = rabbit;
     }
 
